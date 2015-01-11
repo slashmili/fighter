c70285391466bef1886d371c03f6a3a1f2887297
Milad Rastian <eslashmili@gmail.com>
2015-01-11 10:40:04 +0000

    Added php-coveralls

---
+++ t/test_autoload.php
@@ -0,0 +1,11 @@
+<?php
+
+class PHP_Token_TYPELIST_LT extends PHP_Token {}
+class PHP_Token_TYPELIST_GT extends PHP_Token {}
+class PHP_Token_TYPE extends PHP_Token {}
+class PHP_Token_SHAPE extends PHP_Token {}
+class PHP_Token_LAMBDA_OP extends PHP_Token {}
+class PHP_Token_LAMBDA_CP extends PHP_Token {}
+class PHP_Token_LAMBDA_ARROW extends PHP_Token {}
+
+require_once __DIR__ . "/../vendor/autoload.php";
