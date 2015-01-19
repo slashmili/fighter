4324c9d3c31c9113eb0485de9edd96ef40cf2c05
Milad Rastian <eslashmili@gmail.com>
2015-01-19 15:09:20 +0000

    Removed unused file

--- src/Fighter/Util/DynamicAssignment.php
+++
@@ -1,9 +0,0 @@
-<?hh //partial
-
-namespace Fighter\Util;
-
-trait DynamicAssignment {
-    public function setWithDynamicAssignment(string $key, mixed $value): void {
-        $this->$key = $value;
-    }
-}
