8bb975853155f675b56b392d94411db0a13e9160
Farzad Ghanei <farzad.ghanei@gmail.com>
2015-03-15 21:29:51 +0800

    Adds a bucket class to store variables for app and provide lazy loading

---
+++ src/Fighter/Util/Bucket.hh
@@ -0,0 +1,81 @@
+<?hh // strict
+
+namespace Fighter\Util;
+
+class Bucket
+{
+    protected Map<string, mixed> $data = Map {};
+    protected Map<string, mixed> $provider = Map {};
+
+    public function __construct(?KeyedTraversable<string, mixed> $it=null)
+    {
+        $this->data = new Map($it);
+    }
+
+    public function set(string $key, mixed $value) : this
+    {
+        $this->data->set($key, $value);
+        return $this;
+    }
+
+    /**
+     * @throws \InvalidArgumentException on not callable provider
+     * @throws \LogicException on registring with a key that is already set
+     */
+    public function registerProvider(string $key, mixed $callable) : this
+    {
+        if (!is_callable($callable)) {
+            throw new \InvalidArgumentException("Can not register provider for '$key'. Provider is not callable");
+        }
+        if ($this->data->contains($key)) {
+            throw new \LogicException("A value for key '$key' has already been set. Provider will not be used");
+        }
+        $this->provider[$key] = $callable;
+        return $this;
+    }
+
+    public function contains(string $key) : bool
+    {
+        return $this->data->contains($key);
+    }
+
+    public function keys() : Vector<string>
+    {
+        return $this->data->keys()->addAll($this->provider->keys());
+    }
+
+    public function get(string $key) : mixed
+    {
+        if (!$this->data->contains($key) && $this->provider->contains($key)) {
+            $provider = $this->provider[$key];
+            if (!is_callable($provider)) {
+                throw new \UnexpectedValueException("Provider is not a callable for key '$key'");
+            }
+            // UNSAFE
+            $this->data[$key] = call_user_func($provider);
+        }
+        return $this->data->get($key);
+    }
+
+    public function getOrElese(string $key, mixed $default=null) : mixed
+    {
+        return ($this->data->contains($key)) ? $this->data[$key] : $default;
+    }
+
+    public function remove(string $key) : this
+    {
+        $this->data->remove($key);
+        return $this;
+    }
+
+    public function clear() : this
+    {
+        $this->data->clear();
+        return $this;
+    }
+
+    public function toMap() : Map<string, mixed>
+    {
+        return $this->data->toMap();
+    }
+}
